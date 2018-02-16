6d07abbd79d0b54993f86fe3bcbe84061590d1aa
Aether <379756572@qq.com>
2018-02-16 23:41:45 +0800

    from PO to OO...

--- Exercise/app/src/main/java/com/example/android/datafrominternet/LBS/Map.java
+++ Exercise/app/src/main/java/com/example/android/datafrominternet/LBS/Map.java
@@ -17,14 +17,18 @@ import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 import com.example.android.datafrominternet.Notebook.AddNote;
 import com.example.android.datafrominternet.Notebook.NoteData;
 import com.example.android.datafrominternet.R;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -165,38 +169,46 @@ public class Map extends AppCompatActivity {
                     MapStatusUpdate update = MapStatusUpdateFactory.newLatLngZoom(ll,18f);
                     baiduMap.animateMapStatus(update);
 
-                    MyLocationData.Builder locationBuilder = new MyLocationData.Builder();
-                    locationBuilder.latitude(Lat);
-                    locationBuilder.longitude(Lon);
-                    MyLocationData locationdata = locationBuilder.build();
-                    baiduMap.setMyLocationData(locationdata);
-
-                    BmobGeoPoint point = new BmobGeoPoint(Lat, Lon);
-                    pushBmobinfo(point);
-                    findNeighbor(point);
+                    setMyLocation(Lon, Lat);
+                    BmobGeoPoint point = new BmobGeoPoint(Lon, Lat);
+                    setBmobInfo(point);
+                    setOthersLocation(point);
 
                 }
             });
         }
 
     }
+    private void setMyLocation(double Lon, double Lat){
+        MyLocationData.Builder locationBuilder = new MyLocationData.Builder();
+        locationBuilder.latitude(Lat);
+        locationBuilder.longitude(Lon);
+        MyLocationData locationdata = locationBuilder.build();
+        baiduMap.setMyLocationData(locationdata);
+    }
 
-    private String query(String number){
+    private void setBmobInfo(final BmobGeoPoint point){
+
+        SharedPreferences preferences = getSharedPreferences("userinfo",MODE_PRIVATE);
+        final String username = preferences.getString("username",null);
+        final String number = preferences.getString("phonenumber",null);
+
         BmobQuery<LocationData> query = new BmobQuery<LocationData>();
-        query.addWhereEqualTo("phoneNumer", number);
+        query.addWhereEqualTo("phoneNumber", number);
         query.setLimit(1);
-        final String objectId;
         query.findObjects(new FindListener<LocationData>() {
             @Override
             public void done(List<LocationData> object, BmobException e) {
                 if(e==null){
-                    objectId = object.get(0).getObjectId();
+                    String objectId = object.get(0).getObjectId();
+                    update(objectId,point,number,username);
+                    Log.i("bmob",objectId+"haha");
                 }else{
-                    Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
+                    Log.i("bmob","失败："+e.getMessage()+"\n"+e.getErrorCode());
                 }
             }
         });
-        return objectId;
+
     }
 
     private void save(BmobGeoPoint point, String number, String username){
@@ -210,21 +222,37 @@ public class Map extends AppCompatActivity {
             public void done(String objectId, BmobException e) {
                 if(e!=null){
                     Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
-                } else {
                     Toast.makeText(Map.this,"同步修改数据失败",Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    private void findNeighbor(BmobGeoPoint point){
+    private void setOthersLocation(BmobGeoPoint point){
         BmobQuery<LocationData> bmobQuery = new BmobQuery<LocationData>();
         bmobQuery.addWhereWithinKilometers("gpsAdd",point,3.0);
-        bmobQuery.setLimit(6);    //获取最接近用户地点的6条数据
+        bmobQuery.setLimit(10);    //获取最接近用户地点的10条数据
         bmobQuery.findObjects(new FindListener<LocationData>() {
             @Override
             public void done(List<LocationData> object,BmobException e) {
                 if(e==null){
+                    for(int i=0; i<object.size();i++) {
+                        MyLocationData.Builder builder = new MyLocationData.Builder();
+                        builder.latitude(object.get(i).getGpsAdd().getLatitude());
+                        builder.longitude(object.get(i).getGpsAdd().getLongitude());
+                        MyLocationData locationdata = builder.build();
+                        baiduMap.setMyLocationData(locationdata);
+                        MyLocationConfiguration.LocationMode mCurrentMode = MyLocationConfiguration.LocationMode.FOLLOWING;//定位跟随态
+                        //int mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;   //默认为 LocationMode.NORMAL 普通态
+                        //int mCurrentMode = MyLocationConfiguration.LocationMode.COMPASS;  //定位罗盘态
+                        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher);
+
+                        MyLocationConfiguration config = new MyLocationConfiguration(
+                                mCurrentMode, true, mCurrentMarker,
+                                ColorTemplate.VORDIPLOM_COLORS[i%6],ColorTemplate.VORDIPLOM_COLORS[i%6]);
+                        baiduMap.setMyLocationConfiguration(config);
+                    }
+
                     Log.d("test","查询成功：共" + object.size() + "条数据。");
                 }else {
                     Log.d("test","查询失败：" + e.getMessage());
@@ -233,37 +261,27 @@ public class Map extends AppCompatActivity {
         });
     }
 
-    private void update(String objectId, final String number, final String username, final BmobGeoPoint point){
+    private void update(final String objectId, final BmobGeoPoint point, final String number, final String username){
+
         LocationData locationData = new LocationData();
         locationData.setGpsAdd(point);
+
         locationData.update(objectId, new UpdateListener() {
             @Override
             public void done(BmobException e) {
                 if(e==null){
                     Log.i("bmob","更新成功");
-                }else if (e.getErrorCode()==9006){
+                }else if (e.getErrorCode()==9018){
                     save(point,number,username);
+                    Log.i("bmob","save"+e.toString());
                 }else {
+                    Log.i("bmob","更新失败"+e.toString());
                     Toast.makeText(Map.this,"同步地理位置失败",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
     }
-
-    private void pushBmobinfo(BmobGeoPoint point){
-
-        SharedPreferences preferences = getSharedPreferences("userinfo",MODE_PRIVATE);
-        String username = preferences.getString("username",null);
-        String number = preferences.getString("phonenumber",null);
-        String objectId = null;
-        objectId = query(number);
-        update(objectId,number,username,point);
-
-    }
-
-
-
 
 }
 
